3e2d738518e662218dc52b7f5e565e28add6cf7f
julien-milosz <71023561+julien-milosz@users.noreply.github.com>
2020-09-21 15:24:43 -0400

    Finish

--- exercice.py
+++ exercice.py
@@ -1,29 +1,74 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from typing import List
 
 
+
 def convert_to_absolute() -> float:
-    return 0.0
+    
+    mon_nombre = float(input("Veuillez entrez votre nombre : "))
+    if mon_nombre < 0:
+        return mon_nombre * -1
+    else:
+        return mon_nombre
+    
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-
-    return [""]
+    
+    my_list = []
+    
+    for lettre in prefixes:
+        my_list.append(lettre + suffixes)
+    
+    return my_list
 
 
 def prime_integer_summation() -> int:
-    return 0
+    
+    def isPrime(n):
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+            
+        return True
+    
+    my_sum = 0
+    prime_i = 1
+    n_prime = 0;
+    
+    x = []
+    
+    while n_prime < 101:
+        if isPrime(prime_i):
+            my_sum += prime_i
+            n_prime += 1
+            
+            x.append(prime_i)
+
+        else:
+            pass
+        prime_i += 1
+        
+    print("x = ", x, " longeur = ", len(x))
+            
+    return (my_sum-1)
 
 
 def factorial(number: int) -> int:
-    return 0
-
+    
+    if number == 1:
+        return 1
+    else:
+        return number*factorial(number-1)
 
 def use_continue() -> None:
+    
+    for i in range(1,11):
+        if i==5:
+            continue
+        else:
+            print(i)
+        
     pass
 
 
